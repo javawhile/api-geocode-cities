b4e22af7084957f5d1e77b6236dd8cb0ccadde29
Hardik Goel <hardikgoel19@gmail.com>
2023-11-15 16:30:58 +0530

    cron changes

--- src/main/java/da/indian/cities/geo/location/MainApplication.java
+++ src/main/java/da/indian/cities/geo/location/MainApplication.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableScheduling
 public class MainApplication {
 
 	private final Logger logger = LoggerFactory.getLogger(MainApplication.class);
@@ -19,12 +21,11 @@ public class MainApplication {
 		SpringApplication.run(MainApplication.class, args);
 	}
 
-	@Scheduled(cron = "*/1 * * * *")
+	@Scheduled(cron = "0 0/8 * * * ?")
 	public void invokeSelf() {
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = restTemplate.getForEntity("https://cities-latlon.onrender.com/alive", String.class);
-		String output = String.format("%s - %s", System.currentTimeMillis(), response);
-		logger.info(output);
+		logger.info("Self Invoke: {}, status: {}", System.currentTimeMillis(), response.getBody());
 	}
 
 }
